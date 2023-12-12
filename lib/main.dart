5aa27c08fce0ebe7647f3e3bdcf1f2b9681f53ce
paumotta <61042875+paumotta@users.noreply.github.com>
2023-12-12 18:22:22 +0100

    Archivo creado y cosas
    No puedo mas o sea mi cabeza va a estallar

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyBeerApp());
+}
+
+class MyBeerApp extends StatefulWidget {
+  const MyBeerApp({super.key});
+
+  @override
+  State<MyBeerApp> createState() => _MyBeerAppState();
+}
+
+class _MyBeerAppState extends State<MyBeerApp> {
+  int actualPage = 0; 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepOrange,
+        appBar: AppBar(
+            title: const Text('BeerApp'),
+            backgroundColor: const Color.fromARGB(255, 255, 162, 134)),
+    
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color.fromARGB(255, 255, 162, 134),
+          onTap: (index){
+            setState(() {
+              actualPage = index;
+            });
+          },
+          currentIndex: actualPage,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favourites'),
+          ],
+        ),
+      ),
+    );
+  }
+}
