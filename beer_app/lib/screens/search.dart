cd0ede421d4dd4551ca56828a25d48d1ebeeafec
Guillem Aixut <99950345+GuillemAixut@users.noreply.github.com>
2024-01-14 13:17:09 +0100

    search cutre/basico

--- beer_app/lib/screens/search.dart
+++ beer_app/lib/screens/search.dart
@@ -1,60 +1,109 @@
-//import 'package:beer_app/model/beer_type.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatefulWidget {
-  const SearchPage({super.key});
+  const SearchPage({Key? key}) : super(key: key);
 
   @override
   State<SearchPage> createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
+  List<Map<String, dynamic>> allBeer = [
+    {
+      'name': 'ALE1',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE2',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE3',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE4',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE5',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE6',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE7',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE8',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE9',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE10',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE11',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE12',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+    {
+      'name': 'ALE13',
+      'image_url': 'https://images.punkapi.com/v2/2.png',
+      'tagline': 'A Real Bitter Experience.'
+    },
+  ];
+
+  List<Map<String, dynamic>> searchedBeer = [];
+
+  @override
+  void initState() {
+    searchedBeer = allBeer;
+    super.initState();
+  }
+
+  void runFilter(String keyWordEntered) {
+    List<Map<String, dynamic>> results = [];
+    if (keyWordEntered.isEmpty) {
+      results = allBeer;
+    } else {
+      results = allBeer
+          .where((beer) =>
+              beer['name'].toLowerCase().contains(keyWordEntered.toLowerCase()))
+          .toList();
+    }
+
+    setState(() {
+      searchedBeer = results;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    //Listas totalmente privisionales para que se vea y no crear otros archivos
-    List<Map<String, dynamic>> allBeer = [
-      {'name':'ALE1','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE2','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE3','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE4','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE5','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE6','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE7','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE8','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE9','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE10','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE11','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE12','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-      {'name':'ALE13','image_url':'https://images.punkapi.com/v2/2.png','tagline':'A Real Bitter Experience.'},
-    ];
-
-    /* No me esta funcionando y nose ni el porque
-    List<Map<String,dynamic>> searchedBeer=[];
-
-    @override
-      initState(){
-      searchedBeer =allBeer;
-      super.initState();
-    }
-    */
-    void runFilter(String keyWordEntered) {
-      List <Map<String,dynamic>> results = [];
-      if (keyWordEntered.isEmpty){results =allBeer;}
-      else{
-        results =allBeer
-        .where((beer) => 
-        beer['name'].toLowerCase().contains(keyWordEntered.toLowerCase()))
-        .toList();
-      }
-      
-      /*setState(() {
-        searchedBeer=results;
-      });
-      */
-    }
-
     return Scaffold(
-      backgroundColor:Colors.deepOrange,
+      backgroundColor: Colors.deepOrange,
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
@@ -63,25 +112,27 @@ class _SearchPageState extends State<SearchPage> {
               height: 20,
             ),
             TextField(
-              onChanged: (value)=> runFilter(value),
+              onChanged: (value) => runFilter(value),
               decoration: const InputDecoration(
-                labelText: 'Search', suffixIcon: Icon(Icons.search),
+                labelText: 'Search',
+                suffixIcon: Icon(Icons.search),
               ),
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: allBeer.length,
+                itemCount: searchedBeer.length,
                 itemBuilder: (context, index) => Card(
-                  key: ValueKey(allBeer[index]['name']),
+                  key: ValueKey(searchedBeer[index]['name']),
                   color: const Color.fromARGB(255, 224, 104, 68),
                   elevation: 3,
                   margin: const EdgeInsets.symmetric(vertical: 7),
                   child: ListTile(
                     leading: CircleAvatar(
-                      backgroundImage: NetworkImage(allBeer[index]['image_url']),
+                      backgroundImage:
+                          NetworkImage(searchedBeer[index]['image_url']),
                     ),
                     title: Text(
-                      allBeer[index]['name'],
+                      searchedBeer[index]['name'],
                       style: const TextStyle(
                         color: Colors.white,
                         fontSize: 24,
@@ -89,8 +140,8 @@ class _SearchPageState extends State<SearchPage> {
                       ),
                     ),
                     subtitle: Text(
-                      allBeer[index]['tagline'],
-                       style: const TextStyle(
+                      searchedBeer[index]['tagline'],
+                      style: const TextStyle(
                         color: Colors.white,
                         fontSize: 16,
                         fontStyle: FontStyle.italic,
@@ -105,8 +156,8 @@ class _SearchPageState extends State<SearchPage> {
       ),
     );
   }
-  
 }
+
 
 
 /* Pruebas sobre esto con API
